6dad990e5b5a2732cd8b1a856e93274307ab87e1
shravan.shan <shravan.shan@nobroker.in>
2023-08-17 00:51:16 +0530

    my learning first commit

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,18 @@
 import React from "react"
+import logo from "./react-logo.png"
 
 export default function Header() {
     const header = (
         <header>
             <nav className="nav">
-                <img src="./react-logo.png" id="nav-logo"></img>
+                <img src={logo} id="nav-logo" className="App-logo" alt="logo" />
                 <ul className="nav-items">
-                <li>Pricing</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
             </nav>
-            
-        </header>   
+        </header>
     )
     return header;
 }
