48a3a6d083a33b97920b77f88345971e235cfbd0
shravan.shan <shravan.shan@nobroker.in>
2023-08-20 00:00:17 +0530

    single reducer lesson

---
+++ redux-demo/singleReducer.js
@@ -0,0 +1,66 @@
+console.log('Learn Redux!')
+const redux = require('redux');
+
+// Action type
+const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
+
+// Action creator - function returning an action
+const buyCakeAction = () => {
+    // Action
+    return {
+        type: BUY_CAKE,
+        info: 'buying a cake'
+    }
+}
+
+const buyIceCreamAction = () => {
+    return {
+        type: BUY_ICECREAM,
+        info: 'buying an ice-cream'
+    }
+}
+
+const initialState = {
+    numOfCakes: 10,
+    numOfIceCreams: 20
+};
+
+// Reducer takes current state and action to execute the state change
+const buyCakeReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case BUY_CAKE:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1
+            };
+        case BUY_ICECREAM:
+            return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams - 1
+            }
+        default:
+            return state;
+    }
+}
+
+// create Redux store by initializing with Reducer
+const store = redux.legacy_createStore(buyCakeReducer);
+console.log('Initial state', store.getState());
+
+// call back function whenever state is updated in Redux store. Return function will be the unsubscribe method
+const unsubscribe = store.subscribe(() => {
+    console.log('Updated state', store.getState())
+});
+
+// state change happens when invoking action to store
+store.dispatch(buyCakeAction());
+store.dispatch(buyCakeAction());
+store.dispatch(buyCakeAction());
+store.dispatch(buyIceCreamAction());
+store.dispatch(buyIceCreamAction());
+store.dispatch(buyCakeAction());
+
+// unsubscribe will stop calling the call back function whenever state is updated
+unsubscribe();
+console.log('Final state', store.getState());
