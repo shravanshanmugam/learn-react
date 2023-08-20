1934292206fcf52ea44cfad800ac1f3526ca2bdd
shravan.shan <shravan.shan@nobroker.in>
2023-08-20 12:24:30 +0530

    only positive value for num of cakes

--- react-redux/src/redux/cake/CakeReducer.js
+++ react-redux/src/redux/cake/CakeReducer.js
@@ -7,7 +7,8 @@ export const cakeReducer = (state = initialState, action) => {
     case BUY_CAKE:
       return {
         ...state,
-        numOfCakes: state.numOfCakes - 1,
+        numOfCakes:
+          state.numOfCakes > 0 ? state.numOfCakes - 1 : state.numOfCakes,
       };
     default:
       return state;
