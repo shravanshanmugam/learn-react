2819dea993037217d63d5d1c899f4ffa38c66909
shravan.shan <shravan.shan@nobroker.in>
2023-08-26 11:00:38 +0530

    Setup react router first commit

---
+++ react-router-v6/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Learn React Router v6</header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
