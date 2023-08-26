c1f565d7b2f9bca6b19bca140d26ee4f376f15c5
shravan.shan <shravan.shan@nobroker.in>
2023-08-26 14:08:01 +0530

    home page css changes

--- react-router-v6/src/components/Home.js
+++ react-router-v6/src/components/Home.js
@@ -13,14 +13,14 @@ function Home() {
         </nav>
       </header>
       <main>
-        <div className="hero">
+        <div className="hero-text">
           <h2>You got the travel plans, we got the travel vans.</h2>
           <p>
             Add adventure to your life by joining the #vanlife movement. Rent
             the perfect van to make your perfect road trip.
           </p>
-          <button>Find your van</button>
         </div>
+        <button>Find your van</button>
       </main>
       <footer>
         <p>&copy; 2022 #VANLIFE</p>
