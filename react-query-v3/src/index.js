afb295ec818bec2ed8266059e750e1056662948c
shravan.shan <shravan.shan@nobroker.in>
2023-08-27 23:12:20 +0530

    react query project setup

---
+++ react-query-v3/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+function App() {
+  return <h2>Learn React Query</h2>;
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
