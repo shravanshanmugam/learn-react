4f2df914415c66c58190a4a725248c5329604fbd
shravan.shan <shravan.shan@nobroker.in>
2023-08-27 23:41:26 +0530

    useQuery hook to fetch data

--- react-query-v3/src/index.js
+++ react-query-v3/src/index.js
@@ -1,20 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
 import { SuperHeroesPage } from "./pages/SuperHeroesPage";
 import { RQSuperHeroesPage } from "./pages/RQSuperHeroesPage";
 import { HomePage } from "./pages/HomePage";
 import "./index.css";
 
+const queryClient = new QueryClient();
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/super-heroes" element={<SuperHeroesPage />} />
-        <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/super-heroes" element={<SuperHeroesPage />} />
+          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
