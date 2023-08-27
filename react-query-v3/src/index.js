975649c1875e6413e20004074c75e09ea955d974
shravan.shan <shravan.shan@nobroker.in>
2023-08-27 23:27:10 +0530

    fixed project setup errors

--- react-query-v3/src/index.js
+++ react-query-v3/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SuperHeroesPage } from "./pages/SuperHeroesPage";
 import { RQSuperHeroesPage } from "./pages/RQSuperHeroesPage";
 import { HomePage } from "./pages/HomePage";
@@ -10,24 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/super-heroes">Traditional Super Heroes</Link>
-              </li>
-              <li>
-                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-              </li>
-            </ul>
-          </nav>
-          <Route path="/super-heroes" element={<SuperHeroesPage />} />
-          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
-          <Route path="/" element={<HomePage />} />
-        </div>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/super-heroes" element={<SuperHeroesPage />} />
+        <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
       </Routes>
     </BrowserRouter>
   );
--- react-query-v3/src/pages/HomePage.js
+++ react-query-v3/src/pages/HomePage.js
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
 export const HomePage = () => {
-  return <h2>Home Page</h2>;
+  return (
+    <>
+      <h2>Home Page</h2>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/super-heroes">Traditional Super Heroes</Link>
+          </li>
+          <li>
+            <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 };
